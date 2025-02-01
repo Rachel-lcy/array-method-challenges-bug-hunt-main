c19bc298311e3f1d93c1285ffab9683f95518202
Rachel <hurui.lcy@gmail.com>
2025-01-31 21:18:39 -0700

    update

--- bugHunt.js
+++ bugHunt.js
@@ -6,39 +6,45 @@
 
 const characters = [
   { id: 1, name: 'Luke Skywalker', age: 20 },
-  { id: 2, name: 'Darth Vader', age: "50" },
-  { i: 3, name: 'Princess Leia', age: 20 },
-  { id: 4, name "Han Solo", age: 33 }
+  { id: 2, name: 'Darth Vader', age: 50 },
+  { id: 3, name: 'Princess Leia', age: 20 },
+  { id: 4, name: "Han Solo", age: 33 }
 ]
 
 // 2.
 function listUsers(arr) {
-  users.forEach(user => {
-    console.log(user.name)
+  arr.forEach((user) => {
+    // console.log(user.name)
   })
 }
-
+listUsers(characters);
 // test function
 
 // 3. 
 
-function getUserLastName(user) {
-  let lastName = user.split(" ")[0]
-  return lastName
-}
+// function getUserLastName(user) {
+//   let lastName = user.split(" ")[0] 
+//   return lastName
+// }
+function getUserLastName(user = '') {
+  let lastName = user.split(" ").slice(-1)[0]
 
+  return lastName;
+}
+// console.log(getUserLastName("Luke Skywalker"));
 // test function
 
 // 4. 
 
 function checkValidUserName (arr) {
-  return arr.forEach(user => {
-    if (user.name.length <= 0) {
+  arr.forEach(user => {
+    if (!user.name) {
       console.log(user.name, "OK")
     } else {
       console.log(user.name, "BAD")
-    } 
+    }
   })
 }
 
 // test function
+checkValidUserName();
